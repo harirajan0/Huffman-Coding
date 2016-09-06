7bf2ce5188351f888572b1cb4fcd2be2316cf0a2
harirajan0 <hari.rajan@duke.edu>
2016-09-06 11:09:48 -0400

    used in pace of IOException/Runtime Rxception for ease of use

--- HuffException.java
+++ HuffException.java
@@ -1,4 +1,5 @@
 /**
+ *	
  *	Used in place of IOException so user does not have to use
  *	try-catch blocks.  Used in place of RuntimeException so
  *	that regular RuntimeExceptions are still handled in the regular
